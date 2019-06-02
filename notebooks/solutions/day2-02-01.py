ada22261b85255f97d824fedfec91a9be7ab644e
Gilles Louppe <g.louppe@gmail.com>
2019-06-02 22:22:21 +0200

    save

---
+++ notebooks/solutions/day2-02-01.py
@@ -0,0 +1,15 @@
+df["income_bin"] = df["income"] == " >50K"
+
+df.groupby("age")["income_bin"].mean().plot()
+plt.show()
+
+df.groupby("gender")["income_bin"].mean().plot(kind="bar")
+plt.show()
+
+df.groupby("education")["income_bin"].mean().plot(kind="bar")
+plt.show()
+
+df.groupby("race")["income_bin"].mean().plot(kind="bar")
+plt.show()
+
+df.corr()
---
+++ notebooks/solutions/day2-02-02.py
@@ -0,0 +1,2 @@
+X = df.drop(["income"], axis=1)
+y = df["income"]
